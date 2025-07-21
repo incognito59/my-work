e541ffdef898ff9beaef48945d21eb2587c7d489
incognito59 <afolabiprosper061@gmail.com>
2025-07-21 20:47:31 +0100

    Final updates before closing work

--- products/urls.py
+++ products/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('cart/', views.view_cart, name='view-cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('confirm-payment/', views.confirm_payment, name='confirm-payment'),  
+    path('product/<int:product_id>/', views.product_detail, name='product-detail'),
 ]
