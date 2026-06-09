a561a95d9175b5514b75b9e8f95fe9373fc8317f
Afolabi peter <afolabiprosper329@gmail.com>
2026-06-09 16:58:39 +0100

    commit

--- mywork/settings.py
+++ mywork/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'frontend']
+STATICFILES_DIRS = [BASE_DIR / 'frontend', BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
