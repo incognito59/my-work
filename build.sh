695ffa88284735e03f3ad6ab2ec402d01664677b
incognito59 <afolabiprosper061@gmail.com>
2026-06-13 12:36:26 +0000

    changes

--- build.sh
+++ build.sh
@@ -3,14 +3,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata data.json
-python manage.py shell -c "
-from django.contrib.sites.models import Site
-Site.objects.update_or_create(
-    id=1,
-        defaults={
-                'domain': 'retail-logistics-core-t0xz.onrender.com',
-                        'name': 'RedCart'
-                            }
-                            )
-                            print('Site updated')
-                            "
+python manage.py shell -c "from django.contrib.sites.models import Site; Site.objects.update_or_create(id=1, defaults={'domain': 'retail-logistics-core-t0xz.onrender.com', 'name': 'RedCart'})"    "
